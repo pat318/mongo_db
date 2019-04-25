0f3f76d0d09be1c0341c3cdbbbdf152ffaba88b1
Patrick Thompson <pthompson318@gmail.com>
2019-04-25 11:24:29 -0700

    setting up server and controller

---
+++ server.js
@@ -0,0 +1,40 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser"); //JSON responses
+const mongoose = require("mongoose"); //Mongo object modelling 
+const request = require("request"); //Makes http calls
+const cheerio = require("cheerio"); //Scraper
+
+// Require all models
+var db = require("./models");
+
+// Port configuration for local/Heroku
+var PORT = process.env.PORT || process.argv[2] || 8080;
+
+// Initialize Express
+var app = express();
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+// Controllers
+var router = require("./controllers/api.js");
+app.use(router);
+// Connect to the Mongo DB
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+mongoose.Promise = Promise;
+
+
+// Start the server
+app.listen(PORT, function () {
+    console.log(`This application is running on port: ${PORT}`);
+});
